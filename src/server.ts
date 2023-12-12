14da3e78e76e6755d0158f8cd921d0f3b2ac0aec
ZanellaIgor <igor_zanella@outlook.com>
2023-12-11 23:06:20 -0300

    Iniciando Projeto Node

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import https from 'https';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const runServer = (port: number, server: http.Server) => {
+  server.listen(port, () => {
+    console.log(`Running ar PORT ${port}`);
+  });
+};
+
+const regularServer = http.createServer(app);
+if (process.env.NODE_ENV === 'production') {
+  //TODO: Configurar SSL
+  //TODO: Rodar server na 80 e na 443
+} else {
+  const serverPort: number = process.env.PORT
+    ? parseInt(process.env.PORT)
+    : 9000;
+  runServer(serverPort, regularServer);
+}
