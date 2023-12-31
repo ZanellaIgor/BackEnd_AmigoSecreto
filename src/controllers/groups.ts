1bf7f4598712fe4aa363a55239325bbf77c214b4
ZanellaIgor <igor_zanella@outlook.com>
2023-12-31 19:33:57 -0300

    Iniciado Crud Groups

---
+++ src/controllers/groups.ts
@@ -0,0 +1,50 @@
+import { RequestHandler } from 'express';
+import * as groups from '../services/groups';
+import { z } from 'zod';
+export const getAll: RequestHandler = async (req, res) => {
+  const { id_event } = req.params;
+  const decodedIdEvent = decodeURIComponent(id_event);
+  const items = await groups.getAll(parseInt(decodedIdEvent));
+
+  if (items) return res.json({ groups: items });
+
+  res.json({ error: 'Ocorreu um erro' });
+};
+
+export const getGroup: RequestHandler = async (req, res) => {
+  const { id, id_event } = req.params;
+  const goupItem = await groups.getOne({
+    id: parseInt(id),
+    id_event: parseInt(id_event),
+  });
+
+  if (goupItem) return res.json({ group: goupItem });
+
+  res.json({ error: 'Ocorreu um erro' });
+};
+
+export const addGroup: RequestHandler = async (req, res) => {
+  const { id_event } = req.params;
+  const addGroupSchema = z.object({
+    name: z.string(),
+  });
+  const body = addGroupSchema.safeParse(req.body);
+  if (!body.success) return res.json({ error: 'Dados inválidos' });
+
+  const newGroup = await groups.add({
+    ...body.data,
+    id_event: parseInt(id_event),
+  });
+
+  if (newGroup) return res.status(201).json({ group: newGroup });
+  res.json({ error: 'Ocorreu um erro' });
+};
+
+export const updateGroup: RequestHandler = async (req, res) => {
+  const { id, id_event } = req.params;
+  const updateGroupSchema = z.object({
+    name: z.string().optional(),
+  });
+  const body = updateGroupSchema.safeParse(req.body);
+  if (!body.success) return res.json({ error: 'Dados inválidos' });
+};
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import * as auth from '../controllers/auth';
 import * as events from '../controllers/events';
+import * as groups from '../controllers/groups';
 const router = Router();
 
 router.get('/ping', auth.validate, (req, res) =>
@@ -14,4 +15,9 @@ router.get('/events/:id', auth.validate, events.getEvent);
 router.post('/events', auth.validate, events.addEvent);
 router.put('/events/:id', auth.validate, events.updateEvent);
 router.delete('/events/:id', auth.validate, events.deleteEvent);
+
+router.get('/events/:id_event/groups', auth.validate, groups.getAll);
+router.get('/events/:id_event/groups/:id', auth.validate, groups.getGroup);
+router.post('/events/:id_event/groups/', auth.validate, groups.addGroup);
+router.put('/events/:id_event/groups/:id', auth.validate, groups.updateGroup);
 export default router;
